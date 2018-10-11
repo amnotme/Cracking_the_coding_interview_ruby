4496548d32dc7fc31dba6a80f93d28e386805b51
amnotme <amnotme@icloud.com>
2018-10-10 18:03:55 -0700

    implemented naive solution for ex1.6 without a String Builder method

--- ch1_Arrays_strings/06_stringCompression.rb
+++ ch1_Arrays_strings/06_stringCompression.rb
@@ -6,8 +6,36 @@
 #    By: lhernand <lhernand@student.42.us.or>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/10 16:40:55 by lhernand          #+#    #+#              #
-#    Updated: 2018/10/10 16:41:06 by lhernand         ###   ########.fr        #
+#    Updated: 2018/10/10 18:03:26 by lhernand         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+=begin
+String Compression: Implement a method to perform basic string compression 
+using the counts of repeated characters. For example, the string aabcccccaaa 
+would become a2blc5a3. If the "compressed" string would not become smaller 
+than the original string, your method should return the original string. 
 
+You can assume the string has only uppercase and lowercase letters (a - z).
+=end
+
+
+def 		stringCompression(args)
+	i = 1;
+	arr = "";
+	count = 1;
+	len = args.length
+	while i < len + 1
+		if args[i - 1] == args[i]
+			count += 1
+		else
+			arr <<  args[i - 1] <<  count.to_s
+			count = 1
+		end
+		i += 1
+	end
+	arr.size > len ? args : arr
+end
+
+
+puts stringCompression(ARGV[0])
